143b0ac3e9a72774d7dce1c79f0373b4de471c9c
masterchefplus <72683409+masterchefplus@users.noreply.github.com>
2020-10-17 23:22:24 +0700

    Add files via upload

---
+++ precache-manifest.d3c76229a982b5f9e72c7591e5b470e1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "91747f02b8603065a13e602820f9a189",
+    "url": "./static/media/telegramLogo.91747f02.png"
+  },
+  {
+    "revision": "271690b6a365d4d573bb6741bc3f7630",
+    "url": "./static/media/discordLogo.271690b6.png"
+  },
+  {
+    "revision": "d3a8f115f144c07658c6ec16d878680a",
+    "url": "./static/media/TronLinkLogo.d3a8f115.png"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "bddd302af3be56dc78d2",
+    "url": "./static/js/main.6f300cee.chunk.js"
+  },
+  {
+    "revision": "5ed5ff9978fe7a769513",
+    "url": "./static/js/2.3ee1d222.chunk.js"
+  },
+  {
+    "revision": "bddd302af3be56dc78d2",
+    "url": "./static/css/main.34f25eef.chunk.css"
+  },
+  {
+    "revision": "0ab4be6acd362f6475d5f8811fd4d745",
+    "url": "./index.html"
+  }
+];
